d88f85c16faec49c15def4348032ca2267017517
Mbuli Christ <christmb017@gmail.com>
2024-01-09 16:45:39 +0100

    option modifier et supprimer

--- Program.cs
+++ Program.cs
@@ -14,13 +14,17 @@ class Program
         while (true)
         {
             // Menu des options disponibles
-            Console.WriteLine("1. Ajouter une filière");
-            Console.WriteLine("2. Ajouter un enseignant");
-            Console.WriteLine("3. Ajouter un étudiant");
-            Console.WriteLine("4. Afficher toutes les filières");
-            Console.WriteLine("5. Afficher tous les enseignants");
-            Console.WriteLine("6. Afficher tous les étudiants");
-            Console.WriteLine("0. Quitter");
+            Console.WriteLine("\nBienvenue Gestion des filières, enseignants et étudiants en C# ");
+            Console.WriteLine("-------------------------------------------------------------\n");
+            Console.WriteLine("Menu Principale : \n");
+
+
+            Console.WriteLine("1. Ajouter une filière \t 2. Ajouter un enseignant \t 3. Ajouter un étudiant \n");
+            Console.WriteLine("4. Afficher toutes les filières \t 5. Afficher tous les enseignants \t 6. Afficher tous les étudiants \n");
+            Console.WriteLine("7. Supprimer un étudiants \t 8. Modifier un étudiants \n");
+            Console.WriteLine("0. QUITTER");
+            Console.WriteLine("**************************************************************************************************************");
+
 
             int choix = Convert.ToInt32(Console.ReadLine());
 
@@ -44,11 +48,24 @@ class Program
                 case 6:
                     AfficherEtudiants();
                     break;
+                case 7:
+                    SupprimerEtudiant();
+                    break;
+                case 8:
+                    ModifierEtudiant();
+                    break;
+                // case 9:
+                //     SupprimerEnseignant();
+                //     break;
+                // case 10:
+                //     ModifierEnseignant();
+                //     break;
+
                 case 0:
-                    Console.WriteLine("Au revoir !");
+                    Console.WriteLine("\n Au revoir ! \n");
                     return;
                 default:
-                    Console.WriteLine("Choix invalide");
+                    Console.WriteLine("\n Choix invalide \n");
                     break;
             }
         }
@@ -66,32 +83,32 @@ class Program
     // Méthode pour ajouter un enseignant
     static void AjouterEnseignant()
     {
-        Console.WriteLine("Entrez le nom de l'enseignant :");
+        Console.WriteLine("Entrez le nom de l'enseignant :\n");
         string nom = Console.ReadLine();
         enseignants.Add(new Enseignant(nom));
-        Console.WriteLine("Enseignant ajouté avec succès !");
+        Console.WriteLine("Enseignant ajouté avec succès !\n");
     }
 
     // Méthode pour ajouter un étudiant
     static void AjouterEtudiant()
     {
-        Console.WriteLine("Entrez le prénom de l'étudiant :");
+        Console.WriteLine("Entrez le prénom de l'étudiant :\n");
         string prenom = Console.ReadLine();
-        Console.WriteLine("Entrez le nom de l'étudiant :");
+        Console.WriteLine("Entrez le nom de l'étudiant :\n");
         string nom = Console.ReadLine();
-        Console.WriteLine("Entrez l'email de l'étudiant :");
+        Console.WriteLine("Entrez l'email de l'étudiant :\n");
         string email = Console.ReadLine();
-        Console.WriteLine("Entrez le sexe de l'étudiant (M/F) :");
+        Console.WriteLine("Entrez le sexe de l'étudiant (M/F) :\n");
         char sexe = Convert.ToChar(Console.ReadLine());
 
         etudiants.Add(new Etudiant(prenom, nom, email, sexe));
-        Console.WriteLine("Étudiant ajouté avec succès !");
+        Console.WriteLine("Étudiant ajouté avec succès !\n");
     }
 
     // Méthode pour afficher toutes les filières
     static void AfficherFilieres()
     {
-        Console.WriteLine("Liste des filières :");
+        Console.WriteLine("Liste des filières :\n");
         foreach (var filiere in filieres)
         {
             Console.WriteLine(filiere.Nom);
@@ -101,7 +118,7 @@ class Program
     // Méthode pour afficher tous les enseignants
     static void AfficherEnseignants()
     {
-        Console.WriteLine("Liste des enseignants :");
+        Console.WriteLine("Liste des enseignants :\n");
         foreach (var enseignant in enseignants)
         {
             Console.WriteLine(enseignant.Nom);
@@ -111,10 +128,52 @@ class Program
     // Méthode pour afficher tous les étudiants
     static void AfficherEtudiants()
     {
-        Console.WriteLine("Liste des étudiants :");
+        Console.WriteLine("Liste des étudiants :\n");
         foreach (var etudiant in etudiants)
         {
             Console.WriteLine($"{etudiant.Prenom} {etudiant.Nom}, Email: {etudiant.Email}, Sexe: {etudiant.Sexe}");
+        }
+    }
+    // Méthode pour supprimer un étudiant
+    static void SupprimerEtudiant()
+    {
+        Console.WriteLine("Entrez l'email de l'étudiant à supprimer :\n");
+        string email = Console.ReadLine();
+
+        var etudiantASupprimer = etudiants.Find(e => e.Email == email);
+        if (etudiantASupprimer != null)
+        {
+            etudiants.Remove(etudiantASupprimer);
+            Console.WriteLine("Étudiant supprimé avec succès !\n");
+        }
+        else
+        {
+            Console.WriteLine("Étudiant non trouvé.\n");
+        }
+    }
+
+    // Méthode pour modifier un étudiant
+    static void ModifierEtudiant()
+    {
+        Console.WriteLine("Entrez l'email de l'étudiant à modifier : \n");
+        string email = Console.ReadLine();
+
+        var etudiantAModifier = etudiants.Find(e => e.Email == email);
+        if (etudiantAModifier != null)
+        {
+            Console.WriteLine("Entrez le nouveau prénom de l'étudiant :\n");
+            etudiantAModifier.Prenom = Console.ReadLine();
+            Console.WriteLine("Entrez le nouveau nom de l'étudiant :\n");
+            etudiantAModifier.Nom = Console.ReadLine();
+            Console.WriteLine("Entrez le nouvel email de l'étudiant :\n");
+            etudiantAModifier.Email = Console.ReadLine();
+            Console.WriteLine("Entrez le nouveau sexe de l'étudiant (M/F) :\n");
+            etudiantAModifier.Sexe = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Étudiant modifié avec succès !\n");
+        }
+        else
+        {
+            Console.WriteLine("Étudiant non trouvé.");
         }
     }
 }
@@ -144,10 +203,10 @@ class Enseignant
 // Classe Etudiant pour représenter un étudiant
 class Etudiant
 {
-    public string Prenom { get; }
-    public string Nom { get; }
-    public string Email { get; }
-    public char Sexe { get; }
+    public string Prenom { get; set; }
+    public string Nom { get; set; }
+    public string Email { get; set; }
+    public char Sexe { get; set; }
 
     public Etudiant(string prenom, string nom, string email, char sexe)
     {
@@ -157,10 +216,3 @@ class Etudiant
         Sexe = sexe;
     }
 }
-
-
-
-
-
-
-//hgjfhtdghd
